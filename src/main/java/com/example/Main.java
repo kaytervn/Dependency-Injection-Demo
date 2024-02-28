7e1cf366d01af28f11d54166689b444a5cf4d4b4
KayterCantFixBug <118961250+KayterCantFixBug@users.noreply.github.com>
2024-02-28 14:18:31 +0700

    Initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MyApplication app = context.getBean("myApplication", MyApplication.class);
+        app.doWork();
+    }
+}
