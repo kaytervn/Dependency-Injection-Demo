69e6990307df6d96c629b55f8343f9ab2b793702
KayterCantFixBug <118961250+KayterCantFixBug@users.noreply.github.com>
2024-02-28 14:20:42 +0700

    a

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         MyApplication app = context.getBean("myApplication", MyApplication.class);
